3cfcaf95830bba3dc8c98fb3205dcd926996f1e9
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-03-19 22:34:30 -0400

    Added styled component instead and proptypes defined

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,49 @@
-import React from 'react';
-import styles from './Button.module.scss'
-const Button = (props) => (
-    <button onClick={props.onClick} className={styles.btn}>{props.children}</button>
-);
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-export default Button;
+const StyledButton = styled.button`
+  grid-area: start;
+  display: block;
+  position: relative;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  padding: 10px 40px;
+  border-radius: 10px;
+  background: blue;
+  color: white;
+
+  font-size: 16px;
+  font-family: inherit;
+
+  box-shadow: 0 10px #000091;
+
+  &:hover {
+    box-shadow: 0 6px #000091;
+    top: 4px;
+  }
+
+  &:active {
+    box-shadow: 0 2px #000091;
+    top: 8px;
+    transition-duration: 0.2s;
+  }
+`
+
+const Button = ({ onClick, children }) => (
+  <StyledButton onClick={onClick}>
+    {children}
+  </StyledButton>
+)
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+}
+
+export default Button
