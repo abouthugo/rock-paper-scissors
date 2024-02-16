8fd6dee4d471e25d639bcdcff04ea95853903bba
abouthugo <hugo@perdomo.dev>
2024-02-16 11:20:59 -0500

    cleaning up css and adding error message for invalid usernames

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -7,6 +7,7 @@ export default function Welcome() {
 	const navigate = useNavigate();
 	const [state, { setPlayerName }] = useGameState();
 	const [username, setUsername] = createSignal("");
+	const [showError, setShowError] = createSignal(false);
 
 	onMount(() => {
 		if (state.user?.name) {
@@ -15,31 +16,36 @@ export default function Welcome() {
 	});
 	const onSubmit = (e: Event) => {
 		e.preventDefault();
+		if (!username()) {
+			setShowError(true);
+			return;
+		}
 		console.log("reached welcome onion", typeof setPlayerName);
 		setPlayerName(username());
 		navigate("/lobby");
 	};
 	return (
 		<>
-			<h1 class={`${styles.title}`}>Please enter a username</h1>
-			<div class={`${styles.panel}`}>
-				<form
-					onsubmit={onSubmit}
-					action="/game"
-					class="flex gap-2 items-center"
-				>
+			<h1 class={styles.title}>Please enter a username</h1>
+			<div class={styles.panel}>
+				<div class="flex gap-2 items-center">
 					<input
 						type="text"
 						name="username"
 						placeholder="Username"
 						onInput={(e) => setUsername(e.target.value)}
 						required
-						class={`${styles["input-box"]}`}
+						class={styles["input-box"]}
 					/>
-					<button type="submit" class={styles.button}>
+					<button type="button" class={styles.button} onClick={onSubmit}>
 						Go
 					</button>
-				</form>
+				</div>
+				{showError() && (
+					<div class="text-red-600 bg-red-100 rounded-sm flex justify-center py-2">
+						<p class="text-center">Enter a valid username</p>
+					</div>
+				)}
 			</div>
 		</>
 	);
